7d429761b9c2a547d69b1e736b71b0f42f57ea11
dwLG00 <wallace.dylan00@gmail.com>
2024-03-27 19:38:41 -0500

    finished adding gradient computation (I think?)

--- nn.py
+++ nn.py
@@ -1,4 +1,5 @@
 import layers
+import numpy np
 
 class NN:
     def __init__(self, *l):
@@ -6,8 +7,42 @@ class NN:
         self.length = len(self.layer_chain)
         self.intermediate = {}
 
+    def clear(self):
+        self.intermediate = {}
+
     def apply(self, vector):
         k = vector
-        self.intermediate[k] = np.array()
+        intermediate = np.empty((self.length,))
         for i, layer in enumerate(self.layer_chain):
-            k = 
+            k = layer.apply(k)
+            intermediate[i] = k
+        self.intermediate[vector] = intermediate
+        return k
+
+    def compute_grad(self, inputs, exp_out):
+        # initial pass-through
+        n_inputs = inputs.shape[0]
+        grads = [0 for _ in range(n_inputs)]
+
+        for i, vector in enumerate(inputs):
+            exp = exp_out[i]
+            res = apply(vector)
+            error = res - exp
+
+            partial = None
+            for i, layer in enumerate(self.layers[::-1]):
+                if isinstance(layer, WeightLayer):
+                    if not partial:
+                        grad = layer.weight_derivative(vector)
+                    else:
+                        grad = np.tensordot(layer.weight_derivative(vector), partial, 0)
+                    grads[i] += grad
+                if not partial: partial = layer.derivative(vector)
+                else:
+                    partial = np.dot(layer.derivative(vector), partial)
+
+        for i, layer in enumerate(self.layers[::-1]):
+            if isinstance(layer, WeightLayer):
+                layer.apply_grad(2 * grads[i] / n_inputs)
+
+        return grads
