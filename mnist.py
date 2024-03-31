c737645bf9ad139c664494bc88b84e0a3791c919
dwLG00 <wallace.dylan00@gmail.com>
2024-03-31 16:29:44 -0500

    Gonna train mnist. Wish me luck

---
+++ mnist.py
@@ -0,0 +1,8 @@
+# Training a regression model... on a classification problem! Nothing bad can happen.
+
+import os
+import numpy as np
+from keras.datasets import mnist
+
+if __name__ == '__main__':
+    pass
